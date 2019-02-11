3ffa815b60b5569eae7063f6c57c57770314a615
moroztaras <moroztaras@i.ua>
2019-02-11 19:11:14 +0200

    counter svists in profile user

--- src/Controller/BlockController.php
+++ src/Controller/BlockController.php
@@ -22,4 +22,17 @@ class BlockController extends Controller
           'count_svistyns' => $count_svistyns,
         ]);
     }
+
+    public function userCountSvistyn($id)
+    {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+        if (!$user) {
+            return new Response();
+        }
+        $count_svistyns = $this->getDoctrine()->getRepository(Svistyn::class)->counterSvistynsByUser($user);
+
+        return $this->render('User/user_svistyn.html.twig', [
+          'count_svistyns' => $count_svistyns,
+        ]);
+    }
 }

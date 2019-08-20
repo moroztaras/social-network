01ac94cd979a00ec5f09f1853bd7a91f2252a04c
moroztaras <taras.v.moroz@gmail.com>
2019-08-20 14:42:47 +0300

    added context

--- src/Normalizer/UserNormalizer.php
+++ src/Normalizer/UserNormalizer.php
@@ -3,44 +3,37 @@
 namespace App\Normalizer;
 
 use App\Entity\User;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class UserNormalizer implements NormalizerInterface
 {
-    const GROUP_LOGIN = 'Login';
-    const GROUP_REGISTRATION = 'Registration';
-    const GROUP_PROFILE = 'Profile';
-
     /**
-     * @param User $user
-     * @param null $format
+     * @param User  $user
+     * @param null  $format
      * @param array $context
+     *
      * @return array|bool|float|int|string
      */
     public function normalize($user, $format = null, array $context = [])
     {
-        if (isset($context[AbstractNormalizer::GROUPS]) && in_array($this::GROUP_LOGIN, $context[AbstractNormalizer::GROUPS]))
-            return ["apiToken" => $user->getApiToken()];
+        if (isset($context['registration']) || isset($context['login'])) {
+            return [
+                'api_token' => $user->getApiToken(),
+            ];
+        }
 
-        $data = [
+        return [
           'id' => $user->getId(),
           'fullName' => $user->getFullname(),
           'email' => $user->getEmail(),
           'gender' => $user->getGender(),
-          'birthday' => $user->getBirthday()->format("d-m-Y"),
+          'birthday' => $user->getBirthday()->format('d-m-Y'),
           'api_token' => $user->getApiToken(),
         ];
-
-        if (isset($context[AbstractNormalizer::GROUPS]) && in_array($this::GROUP_REGISTRATION, $context[AbstractNormalizer::GROUPS]))
-            $data["apiToken"] = $user->getApiToken();
-
-        return $data;
     }
 
     public function supportsNormalization($user, $format = null)
     {
         return $user instanceof User;
     }
-
 }
